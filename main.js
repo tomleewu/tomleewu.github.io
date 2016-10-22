44bc8a5a31ade819d53e051f297284f1e026d71a
tomleewu <tlw684@utulsa.edu>
2016-10-22 17:22:34 -0500

    Added projects and smooth scroll to each section

--- main.js
+++ main.js
@@ -2,5 +2,16 @@ $(document).ready(function(){
 	$(".navbar-nav").hide();
 	$(".navbar-nav").delay(700).fadeIn(900);
 
-
+	//Smooth scroll to about section
+	$("#aboutScroll").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 1100);
+});
+	//Smooth scroll to project section
+$("#projectScroll").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#projects").offset().top
+    }, 1100);
+});
 });
